888315b8ffb15dea63af4e3c7fa09de71a7e0720
lukaszub <lukaszub877@gmail.com>
2019-07-10 08:41:45 +0200

    Implemented employees controller

--- app/controllers/employees_controller.rb
+++ app/controllers/employees_controller.rb
@@ -1,20 +1,48 @@
 class EmployeesController < ApplicationController
+	before_action :set_employee, only: [:show, :edit, :update, :destroy]
+
 	def new
 		@employee = current_user.employees.build
 	end
 
+	def index
+    @employee = Employee.where(user_id: current_user)  
+ 	end
+
+ 	def show
+  end
+
 	def create
 		@employee = current_user.employees.build(employee_params)	
 		if @employee.save
-			flash[:success] = "Employee created!"
-			redirect_to root_url
+			redirect_to employees_path, notice: "Employee created!"
 		else
 			render 'new'
 		end
 	end
 
+	def edit  
+ 	end
+
+ 	def update
+    if @employee.update(employee_params) 
+      redirect_to employees_path, notice: "Client details updated."
+    else
+      render 'new'
+    end 
+  end
+
+  def destroy
+    @employee.destroy
+    redirect_to clients_path, notice: "Client deleted." 
+  end
+
 
 	private
+
+			def set_employee
+				@employee = Employee.find(params[:id])	
+			end
 
 			def employee_params
 				params.require(:employee).permit(:first_name, :second_name, :position)	
